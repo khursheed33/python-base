f3386cf4de09f7cfc44e7941fd9fe7af093d6a8d
khursheed33 <khursheed.gaddi@e-zest.in>
2024-04-18 13:02:30 +0530

    OpenAI embedding added

--- app/llm/langchain_openai_manager.py
+++ app/llm/langchain_openai_manager.py
@@ -4,6 +4,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain, LLMChain
 from langchain.output_parsers import StructuredOutputParser
 from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import OpenAIEmbeddings
 from app.utils.utility_manager import UtilityManager
 from app.enums.env_keys import EnvKeys
 
@@ -11,19 +12,27 @@ from app.enums.env_keys import EnvKeys
 class LangchainOpenAIManager(UtilityManager):
     def __init__(self):
         super().__init__()
+        
         self.OPENAI_KEY = self.get_env_variable(EnvKeys.APP_OPENAI_KEY.value)
-        self.TEMPERATURE = self.get_env_variable(
-            EnvKeys.APP_OPENAI_TEMPERATURE.value)
+        self.TEMPERATURE = self.get_env_variable(EnvKeys.APP_OPENAI_TEMPERATURE.value)
+        self.MODEL = self.get_env_variable(EnvKeys.APP_OPENAI_MODEL.value)
+        self.OPENAI_VERBOSE = self.get_env_variable(EnvKeys.APP_OPENAI_VERBOSE.value)
+        
+        os.environ["OPENAI_API_KEY"] = self.OPENAI_KEY
+        
         self.llm_model = ChatOpenAI(
-            model_name=self.OPENAI_KEY,
+            model_name=self.MODEL,
             temperature=self.TEMPERATURE
         )
 
+        self.embedding = OpenAIEmbeddings(openai_api_key=self.get_env_variable(EnvKeys.APP_OPENAI_KEY.value), chunk_size=2000)
+
+        
         self.conversation_memory = ConversationBufferMemory(
             llm=self.llm_model)
 
         self.conversation_chain = ConversationChain(
-            llm=self.llm_model, memory=self.conversation_memory, verbose=False)
+            llm=self.llm_model, memory=self.conversation_memory, verbose=self.OPENAI_VERBOSE)
 
     def run_conversational_chain(self, prompt: str, output_parser: StructuredOutputParser = None) -> dict:
         """
