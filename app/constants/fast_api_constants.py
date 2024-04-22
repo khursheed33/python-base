6f7b0b635092fafdd764ca65b4b3e5e49654b40d
khursheed33 <khursheed.gaddi@e-zest.in>
2024-04-22 16:09:56 +0530

    API docs updated

--- app/constants/fast_api_constants.py
+++ app/constants/fast_api_constants.py
@@ -1,26 +1,28 @@
 class FastAPIConstants:
-    TITLE="Python Base | Server",
+    TITLE="Python Base | Server"
     DESCRIPTION="""
-            A python base with all required components already created, Provides great support for Generative AI. 🚀
-            ## Items
-            # Generative AI wrapper
-             - Langchain Wrapper already created along with various LLM models.
-            # Databases wrapper
-             - Databases wrapper includes MongoDB, PostgreSQL, MSSQL and Local.
-             
-            ## APIs
-            You will be able to:
-            * **Create users** (_not implemented_).
-            * **Read users** (_not implemented_).
-            """,
-    SUMMARY="Summary of the project",
-    VERSION="1.0.0",
-    T_N_C="https://github.com/khursheed33",
+Welcome to the Python Base Server with FastAPI! This base server is designed to provide you with all the necessary components to kickstart your development process. Whether you're building a web application, API, or leveraging Generative AI, this base server has got you covered.
+
+## Features
+
+### Generative AI Wrapper
+- **Langchain Wrapper**: Includes various Language Model (LLM) wrappers, allowing you to easily integrate Generative AI capabilities into your applications. With pre-built models and utilities, you can harness the power of Generative AI with ease.
+
+### Database Wrapper
+- **MongoDB**: Utilize MongoDB, a popular NoSQL database, for flexible and scalable data storage.
+- **PostgreSQL**: Benefit from the robustness and reliability of PostgreSQL, a powerful open-source relational database system.
+- **MSSQL**: Seamlessly integrate with Microsoft SQL Server for enterprise-grade database management.
+- **Local**: Access a lightweight local database option for rapid development and testing.
+
+    """
+    SUMMARY="Project Summary"
+    VERSION="1.0.0"
+    T_N_C="https://github.com/khursheed33"
     CONTACT={
         "name": "Khursheed",
         "url": "https://github.com/khursheed33",
         "email": "gaddi33khursheed@gmail.com",
-    },
+    }
     LICENSE_INFO={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
