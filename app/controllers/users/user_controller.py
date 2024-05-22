39226e8bf0eec2dfe4fbd9c9514ec83bc13a22e2
khursheed33 <gaddi33khursheed@gmail.com>
2024-05-22 23:33:18 +0530

    Server Traffic Test

--- app/controllers/users/user_controller.py
+++ app/controllers/users/user_controller.py
@@ -1,15 +1,15 @@
 from app.databases.sqlite_database_manager import SQLiteDBManager
-from app.models.user_model import UserModel
+from app.models.all_models import UserModel, ResponseModel
 
 class UserController(SQLiteDBManager):
     def __init__(self) -> None:
         super().__init__()
         pass
     
-    def create_new_user(self, userModel: UserModel):
-        print("Creating....")
+    def create_new_user(self, userModel: UserModel) -> ResponseModel:
+        return ResponseModel(message="It's a dummy message after creating user")
         
     def get_all_users(self):
-        print("All Users....")
+        return ResponseModel(message="It's a dummy message get all users")
     def get_users_by_id(self, id:str):
-        print("Creating....")
+        return ResponseModel(message="It's a dummy message get user by id")
